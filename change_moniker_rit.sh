8d37caa1e76834eb879bc0da8c73e7b3a8f14b51
KrakeNocto <octo2kraken@gmail.com>
2024-08-15 21:31:39 +0300

    Update change_moniker_rit.sh

--- change_moniker_rit.sh
+++ change_moniker_rit.sh
@@ -4,5 +4,3 @@ read -p "Enter val address: " VAL
 Moniker=$(/home/ritual/.0gchain/cosmovisor/upgrades/v0.3.1/bin/0gchaind query staking validator $VAL --output json | jq -r '.description.moniker')
 
 sed -i "s/^moniker = .*/moniker = \"$Moniker\"/" /home/ritual/.0gchain/config/config.toml
-
-systemctl restart ogd && journalctl -fu ogd
