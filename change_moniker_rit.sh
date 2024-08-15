356e0e746ef1bd0a3b8315ea9da0df94dc16ddfa
KrakeNocto <octo2kraken@gmail.com>
2024-08-15 20:17:09 +0300

    Add files via upload

---
+++ change_moniker_rit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+Moniker=$(/home/ritual/.0gchain/cosmovisor/upgrades/v0.3.1/bin/0gchaind query staking validator 0gvaloper1uduv77feqv4e9k5j6q2hgu0lmx4renwwwa42gc --output json | jq -r '.description.moniker')
+
+sed -i "s/^moniker = .*/moniker = \"$Moniker\"/" /home/ritual/.0gchain/config/config.toml
+
+systemctl restart ogd && journalctl -fu ogd
